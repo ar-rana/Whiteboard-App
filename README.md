e5d3a37a320a5dd5150d62760d48e09debacf9c7
Aryan Rana <aryan.rana2005@gmail.com>
2025-02-17 22:43:26 +0530

    initialized frontend

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Whiteboard-App
+
+Project Under Progress
---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import React from 'react';
+
+function App() {
+  return (
+    <div className="">
+      <h3 className="text-3xl text-green-500 font-bold">Hello World!!</h3>
+    </div>
+  );
+}
+
+export default App;
