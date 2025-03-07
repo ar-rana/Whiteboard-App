018b0d9ea8fd1068c3bd2dfe3eb803a9d3916620
Aryan Rana <aryan.rana2005@gmail.com>
2025-03-07 16:13:01 +0530

    added object models

---
+++ wsServer/src/main/java/com/practice/wsServer/Model/DrawObject.java
@@ -0,0 +1,16 @@
+package com.practice.wsServer.Model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DrawObject {
+    String color;
+    List<Integer> start;
+    List<Integer> end;
+}
---
+++ wsServer/src/main/java/com/practice/wsServer/Model/EraseObject.java
@@ -0,0 +1,14 @@
+package com.practice.wsServer.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EraseObject {
+    int xOffset;
+    int yOffset;
+    int eraser;
+}
