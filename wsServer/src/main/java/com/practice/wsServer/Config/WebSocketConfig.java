556422dc6506fdf72f07ef77340ed59708eee211
Aryan Rana <aryan.rana2005@gmail.com>
2025-03-07 16:27:18 +0530

    added ws config

---
+++ wsServer/src/main/java/com/practice/wsServer/Config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.practice.wsServer.Config;
+
+import jakarta.websocket.OnError;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/board"); // for messages form server to client
+        registry.setApplicationDestinationPrefixes("/app"); // for messages from client to server
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry endpointRegistry) {
+        endpointRegistry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:3000/whiteboard")
+                .withSockJS();
+    }
+}
